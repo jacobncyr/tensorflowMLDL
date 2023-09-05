9b2bdf567cef32db13e9040519a80d873437d6af
jacob cyr <jacobncyr@outlook.com>
2023-09-05 09:05:47 -0600

    initial commit

---
+++ tensorflow_example_1.py
@@ -0,0 +1,6 @@
+#import tensorflow
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras import models
+from tensorflow.keras import layers
+from tensorflow.keras.utils import to_categorical
+
